6fd71ab7bc12c6907007baca9f07b2e2cdc17af7
BoysheO <boysheo@outlook.com>
2025-06-01 14:50:20 +0800

    Fix CICD

--- BoysheO.UnityLibrary.Dotnet/ResolveTag/Program.cs
+++ BoysheO.UnityLibrary.Dotnet/ResolveTag/Program.cs
@@ -1,17 +1,10 @@
 ﻿// Console.WriteLine("Hello, World!");
 // Console.WriteLine(args.JoinAsOneString(","));
-try
-{
-    var tagName = args[0];
-    var sp = tagName.Split("_");
-    var packName = sp[0];
-    var version = sp[1];
+var tagName = args[0];
+var sp = tagName.Split("_");
+var packName = sp[0];
+var version = sp[1];
 
-    Console.WriteLine($"PACKAGE_NAME={packName}");
-    Console.WriteLine($"PACKAGE_VER={version}");
-    Console.WriteLine($"PROJECT_DIR=BoysheO.UnityLibrary.Dotnet/{packName}");
-}
-catch (Exception exception)
-{
-    Console.WriteLine($"Exception={exception}");
-}
+Console.WriteLine($"PACKAGE_NAME={packName}");
+Console.WriteLine($"PACKAGE_VER={version}");
+Console.WriteLine($"PROJECT_DIR=BoysheO.UnityLibrary.Dotnet/{packName}");
